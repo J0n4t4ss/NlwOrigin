baf4f2beacf1ab34ee3e4edb94b0e774dc6fb2c6
Jonatas Valerio <jonatasvalerio13@hotmail.com>
2021-07-01 15:26:49 -0300

    finishing js

--- main.js
+++ main.js
@@ -1,114 +1,110 @@
-//open and close menu when you click on icon
-
+// open and close the menu when you click in the icon 
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
-for (const element of toggle){
-    element.addEventListener('click', function (){
-        nav.classList.toggle('show')
-    })
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
 }
 
-//when you click in a menu icon, hidden menu
-
+//hidden the menu when you click
 const links = document.querySelectorAll('nav ul li a')
 
-for (const link of links){
-    link.addEventListener('click', function(){
-        nav.classList.remove('show')
-    })
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
 }
 
-//header scroll
-
+//change the header page when you scroll
 const header = document.querySelector('#header')
 const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function(){
-    if(window.scrollY >= navHeight){
-        header.classList.add('scroll')
-    } else{
-        header.classList.remove('scroll')
-    }
-})
-
-//TESTIMONIALS CAROUSEL
-
-const swiper = new Swiper('.swiper-container', {
-   slidesPerView: 1,
-   pagination: {
-       el:'.swiper-pagination'
-   },
-   mousewheel: true,
-   Keyboard: true,
-   breakpoints:{
-       767:{
-           slidesPerView:2,
-           setWraperSize:true
-       }
-   }
-  });
-
-//ScrollReveal
-
-const scrollReveal = ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 700,
-    reset: true
-})
-
-scrollReveal.reveal(`
-    #home .image, #home .text,
-    #about .image, #about .text,
-    #services header, #services .cards,
-    #testimonials header, #testimonials .testimonials,
-    #contact .text, #contact .links,
-    footer .brand, footer .social
-    `,
-     {interval: 100}
-     )
-
-
-//active link
-
-const section = document.querySelectorAll('main section[id]')
-function activateMenuAtCurrentSection(){
-    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
-
-    for( const section of sections){
-        const sectionTop = section.offsetTop
-        const sectionHeight = section.offsetHeight
-        const sectionId = section.getAttribute('id')
-
-        const checkpointStart = checkpoint >= sectionTop
-        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
-
-        if(checkpointStart && checkpointEnd){
-            document
-                .querySelector('nav ul li a[href*=' + sectionId + ']')
-                .classList.add('active')
-        } else{
-            document
-                .querySelector('nav ul li a[href*=' + sectionId + ']')
-                .classList.remove('active')
-        }
-
-    }
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll its higher then the header
+    header.classList.add('scroll')
+  } else {
+    //scroll its lower then the header
+    header.classList.remove('scroll')
+  }
 }
 
-//BACK TO TOP
-
-const backToTopButton = document.querySelector('.back-to-top')
-window.addEventListener('scroll', function(){
-    if(window.scrollY >= 560) {
-        backToTopButton.classList.add('show')
-    } else {
-        backToTopButton.classList.remove('show')
+//Testimonials carousel slider swiper 
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
     }
+  }
 })
 
+//ScrollReveal: show elements when scroll the page 
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
 
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
 
+//back to top 
+const backToTopButton = document.querySelector('.back-to-top')
 
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
 
+//active menu link 
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+// When Scroll 
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
